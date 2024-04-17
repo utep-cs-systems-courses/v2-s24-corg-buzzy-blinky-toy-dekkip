c685c94398fa1b221bd29b47426b708158b49f1e
dekkip <dekipeltshogcodes@gmail.com>
2024-04-17 01:21:32 -0600

    switch

--- project/src/main.c
+++ project/src/main.c
@@ -5,7 +5,7 @@
 #define LED_GREEN BIT0             // P1.6
 #define LEDS (BIT0 | BIT6)
 
-#define SW1 BIT3		/* switch1 is p1.3 */
+#define SW1 BIT0		/* switch1 is p1.3 */
 #define SWITCHES SW1		/* only 1 switch on this board */
 
 void main(void) 
@@ -15,10 +15,10 @@ void main(void)
   P1DIR |= LEDS;
   P1OUT &= ~LEDS;		/* leds initially off */
   
-  P1REN |= SWITCHES;		/* enables resistors for switches */
-  P1IE |= SWITCHES;		/* enable interrupts from switches */
-  P1OUT |= SWITCHES;		/* pull-ups for switches */
-  P1DIR &= ~SWITCHES;		/* set switches' bits for input */
+  P2REN |= SWITCHES;		/* enables resistors for switches */
+  P2IE |= SWITCHES;		/* enable interrupts from switches */
+  P2OUT |= SWITCHES;		/* pull-ups for switches */
+  P2DIR &= ~SWITCHES;		/* set switches' bits for input */
 
   or_sr(0x18);  // CPU off, GIE on
 } 
@@ -26,14 +26,14 @@ void main(void)
 void
 switch_interrupt_handler()
 {
-  char p1val = P1IN;		/* switch is in P1 */
+  char p2val = P2IN;		/* switch is in P1 */
 
 /* update switch interrupt sense to detect changes from current buttons */
-  P1IES |= (p1val & SWITCHES);	/* if switch up, sense down */
-  P1IES &= (p1val | ~SWITCHES);	/* if switch down, sense up */
+  P2IES |= (p2val & SWITCHES);	/* if switch up, sense down */
+  P2IES &= (p2val | ~SWITCHES);	/* if switch down, sense up */
 
 /* up=red, down=green */
-  if (p1val & SW1) {
+  if (p2val & SW1) {
     P1OUT |= LED_RED;
     P1OUT &= ~LED_GREEN;
   } else {
@@ -46,8 +46,8 @@ switch_interrupt_handler()
 /* Switch on P1 (S2) */
 void
 __interrupt_vec(PORT1_VECTOR) Port_1(){
-  if (P1IFG & SWITCHES) {	      /* did a button cause this interrupt? */
-    P1IFG &= ~SWITCHES;		      /* clear pending sw interrupts */
+  if (P2IFG & SWITCHES) {	      /* did a button cause this interrupt? */
+    P2IFG &= ~SWITCHES;		      /* clear pending sw interrupts */
     switch_interrupt_handler();	/* single handler for all switches */
   }
 }
